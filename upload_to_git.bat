fe02a7702d03b9e0e7de2d8b0d5a2ed3825fe23e
Дамир <damirakhtareev0511@gmail.com>
2026-09-08 01:53:26 +0700

    Сохранение конфигурации для задачи: bilet_01 (08.09.2026  1:53:26,50)

--- upload_to_git.bat
+++ upload_to_git.bat
@@ -1,32 +1,47 @@
 @echo off
 chcp 65001 > nul
 
-:: --- НАСТРОЙКИ ---
+:: =================== НАСТРОЙКИ ПУТЕЙ ===================
 set PLATFORM="C:\Program Files (x86)\1cv8t\8.3.27.1508\bin\1cv8t.exe"
 set BASE_PATH="C:\Users\Дамир\OneDrive\Документы\OperAccounting"
-set EXPORT_DIR="C:\Users\Дамир\билеты_по_платформе_решение"
+set EXPORT_ROOT="C:\Users\Дамир\билеты_по_платформе_решение"
 set DB_USER=""
 set DB_PASS=""
+:: =======================================================
 
-echo === 0. Очистка папки от старых файлов ===
-:: Переходим в папку проекта
-cd /d %EXPORT_DIR%
-:: Удаляем все папки, кроме скрытой .git
-for /d %%i in (*) do rmdir /s /q "%%i"
-:: Удаляем все файлы, кроме .gitignore и .bat
-for %%i in (*) do if not "%%i"==".gitignore" if not "%%i"=="upload_to_git.bat" del /q "%%i"
+echo ===================================================
+echo   АВТОМАТИЧЕСКОЕ СОХРАНЕНИЕ БИЛЕТА В GIT
+echo ===================================================
+echo.
+:: Скрипт останавливается и ждет, пока ты введешь имя папки (например: Bilet_01)
+set /p TASK_NAME="Введи название задачи или номер билета (БЕЗ ПРОБЕЛОВ, латиницей или цифрами, например Bilet_01): "
 
-echo === 1. Выгрузка конфигурации из 1С в файлы... ===
-%PLATFORM% DESIGNER /F %BASE_PATH% /N %DB_USER% /P %DB_PASS% /DumpConfigToFiles %EXPORT_DIR% /Out "%EXPORT_DIR%\1c_logs.txt" -NoTruncate
+:: Формируем точный путь к изолированной папке для этого билета
+set EXPORT_DIR=%EXPORT_ROOT%\%TASK_NAME%
 
-echo === 2. Подготовка файлов в Git... ===
+echo.
+echo === Шаг 1. Изолированная очистка папки для %TASK_NAME%... ===
+:: Если папка для этого билета уже была, очищаем только её, другие билеты скрипт НЕ тронет!
+if exist "%EXPORT_DIR%" (
+    for /d %%i in ("%EXPORT_DIR%\*") do rmdir /s /q "%%i"
+    for %%i in ("%EXPORT_DIR%\*") do del /q "%%i"
+) else (
+    mkdir "%EXPORT_DIR%"
+)
+
+echo === Шаг 2. Выгрузка конфигурации 1С в папку %TASK_NAME%... ===
+%PLATFORM% DESIGNER /F %BASE_PATH% /N %DB_USER% /P %DB_PASS% /DumpConfigToFiles "%EXPORT_DIR%" /Out "%EXPORT_ROOT%\1c_logs.txt" -NoTruncate
+
+echo === Шаг 3. Индексация файлов в Git... ===
+cd /d %EXPORT_ROOT%
 git add .
 
-echo === 3. Создание коммита... ===
-git commit -m "Авто-коммит от %date% %time%"
+echo === Шаг 4. Создание коммита для %TASK_NAME%... ===
+git commit -m "Сохранение конфигурации для задачи: %TASK_NAME% (%date% %time%)"
 
-echo === 4. Отправка на удаленный сервер... ===
+echo === Шаг 5. Отправка на Гит... ===
 git push origin main
 
-echo === ГОТОВО! ===
+echo.
+echo === ВСЁ ГОТОВО! Задача %TASK_NAME% успешно изолирована и отправлена ===
 pause
